b0f156fb2cc45022aeb5900af583b7137f13748c
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-09-29 11:46:36 -0400

    Move Node class to subclass of LinkedList

--- lib/main.rb
+++ lib/main.rb
@@ -4,6 +4,8 @@ linked_list = LinkedList.new
 
 linked_list.prepend(11)
 linked_list.append(13)
+linked_list.prepend(10)
 
 puts linked_list.head.data
 puts linked_list.head.next_node.data
+puts linked_list.tail.data
